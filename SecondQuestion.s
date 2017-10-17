f4320df937e67c5c0f6fa6f489b24d41c8829fb4
Jianyun Wu <jianyunwu@Jianyuns-MacBook-Pro.local>
2017-10-17 18:20:45 +0800

    修改了第二题的代码

--- SecondQuestion.s
+++ SecondQuestion.s
@@ -1,10 +1,10 @@
             .data
-buf         .space 100
-f_massage:  .asciiz "\r\nFail!!!\r\n"
-s_massage:  .asciiz "\r\nSuccess!!! Location:"
-n:          .asciiz "\r\n"
-
-            .txt
+buf:        .space 100
+f_massage:  .asciiz "\r\nFail!\r\n"
+s_massage:  .asciiz "\r\nSuccess! Location:"
+newline:    .asciiz "\r\n"
+over:       .asciiz "\r\n程序结束"
+            .text
             .globl main
 main:
             la $a0,buf  #address of input buffer
@@ -15,12 +15,12 @@ main:
 input:      li $v0, 12  #read character
             syscall
             beq $v0,'?',exit    #Branch to lab if src1 = src2
-            add $s1 $0,$0   #use register $s1 to record the location of the character we have found, and initialize it to 0
+            add $t1 $0,$0   #use register $s1 to record the location of the character we have found, and initialize it to 0
 
-loop:       lb  $t1, buf($s1)		# 
+loop:       lb  $t2, buf($t1)		# 
             beq $t2,'\n',fail       #if we found '\n',we failed.
-            beq $v0,$t1,success 
-            addi $s1,$s1,1			# $s1 = $s1 + 1
+            beq $v0,$t2,success 
+            addi $t1,$t1,1			# $s1 = $s1 + 1
             j	loop				# jump toloop
             
 fail:       la  $a0,f_massage
@@ -31,10 +31,16 @@ fail:       la  $a0,f_massage
 success:    la  $a0,s_massage
             li  $v0,4
             syscall
-            add $a0,$s1,$0
+            addi $a0,$t1,1
             li  $v0,1
+            syscall
+            la $a0,newline
+            li $v0, 4		# $v0 = 4
             syscall
             j   input				# jump to    input
 
-exit:       li $v0,10
+exit:       li $v0, 4  
+            la $a0, over    #结束时先换行，再输出名字和ID  
+            syscall  
+            li $v0,10
             syscall
